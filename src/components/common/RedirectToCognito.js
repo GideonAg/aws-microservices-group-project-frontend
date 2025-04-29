711aa6762b1dab529f17baadb9d5d919cab79b27
GideonAg <gideonagbosu72@gmail.com>
2025-04-29 08:56:36 +0000

    Modify cognito redirect url

--- src/components/common/RedirectToCognito.js
+++ src/components/common/RedirectToCognito.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 const RedirectToCognito = () => {
   useEffect(() => {
     window.location.replace(
-      "https://testnameblahblah.auth.eu-central-1.amazoncognito.com/login?client_id=74rihsigpicvreq7m6o2vedkpg&redirect_uri=https://main.daqvdg9hz9mfz.amplifyapp.com/dashboard/&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile"
+      "https://amalitech-task-manager-prod.auth.eu-central-1.amazoncognito.com/login?client_id=23lgp1hf1peqg8s988medcjd10&response_type=token&scope=email+openid+profile+phone+aws.cognito.signin.user.admin&redirect_uri=https://main.daqvdg9hz9mfz.amplifyapp.com/dashboard/"
     );
   }, []);
 
